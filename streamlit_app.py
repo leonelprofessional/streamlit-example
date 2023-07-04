3ca063c6e527152409031c536d03a1f08e51f98c
leonelprofessional <116814941+leonelprofessional@users.noreply.github.com>
2023-07-04 17:45:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,16 +14,4 @@ data = airline_data.sample(n=500, random_state=42)
 # Pie Chart Creation
 fig = px.pie(data, values='Flights', names='DistanceGroup', title='Distance group proportion by flights')
 # Create a dash application
-app = dash.Dash(__name__)
-# Get the layout of the application and adjust it.
-# Create an outer division using html.Div and add title to the dashboard using html.H1 component
-# Add description about the graph using HTML P (paragraph) component
-# Finally, add graph component.
-app.layout = html.Div(children=[html.H1('Airline Dashboard', style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
-                                html.P('Proportion of distance group (250 mile distance interval group) by flights.', style={'textAlign':'center', 'color': '#F57241'}),
-                                dcc.Graph(figure=fig),
-                                               
-                    ])
-# Run the application                   
-if __name__ == '__main__':
-    app.run_server()
+
